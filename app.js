0324e1d29f8c120cea0ccacf0cd2139feb946a15
Kenneth Bacaltos <135862069+KennethB09@users.noreply.github.com>
2023-08-21 22:43:19 +0800

    My 8th reupload
    I hope It truly work now!

---
+++ app.js
@@ -0,0 +1,32 @@
+const navBar = document.getElementById('navBar');
+const toggleBtnM = document.getElementById('mobileToggleBtn');
+const iconBtnToggle = document.getElementById('toggleBtnIcon');
+
+toggleBtnM.addEventListener('click', () => {
+    const navVisible = navBar.getAttribute('data-visible');
+
+    if (navVisible === 'false') {
+        navBar.setAttribute('data-visible', true);
+        toggleBtnM.setAttribute('aria-expanded', true);
+        iconBtnToggle.src = '/news-homepage-main/assets/images/icon-menu-close.svg'
+    } else if (navVisible === 'true') {
+        navBar.setAttribute('data-visible', false);
+        toggleBtnM.setAttribute('aria-expanded', false);
+        iconBtnToggle.src = '/news-homepage-main/assets/images/icon-menu.svg'
+    }
+});
+
+function heroChangeImg() {
+    const image = document.getElementById('heroImage');
+    const viewPortWidth = window.innerWidth || document.documentElement.clientWidth;
+
+    if (viewPortWidth <= 430) {
+        image.src = '/news-homepage-main/assets/images/image-web-3-mobile.jpg';
+    } else if (viewPortWidth > 430) {
+        image.src = '/news-homepage-main/assets/images/image-web-3-desktop.jpg';
+    }
+};
+
+heroChangeImg();
+
+window.addEventListener('resize', heroChangeImg);
